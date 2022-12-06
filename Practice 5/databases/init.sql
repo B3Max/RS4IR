8e8ab035fc27130e9082725bfcb994dad3ef4971
B3Max <B3dMaxOsp@yandex.ru>
2022-12-06 14:17:16 +0300

    WTF????

--- Practice 5/databases/init.sql
+++ Practice 5/databases/init.sql
@@ -2,77 +2,27 @@ CREATE DATABASE IF NOT EXISTS appDB;
 CREATE USER IF NOT EXISTS 'user'@'%' IDENTIFIED BY 'password';
 GRANT SELECT,UPDATE,INSERT,DELETE ON appDB.* TO 'user'@'%';
 FLUSH PRIVILEGES;
+set names 'utf8';
 
 USE appDB;
-CREATE TABLE IF NOT EXISTS users (
-  ID INT(11) NOT NULL AUTO_INCREMENT,
-  name VARCHAR(20) NOT NULL,
-  password VARCHAR(40) NOT NULL,
-  PRIMARY KEY (ID)
+
+CREATE TABLE `orders` (
+	`orderID` INT NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(200),
+	`order` VARCHAR(200),
+	PRIMARY KEY (`orderID`)
 );
+INSERT INTO orders VALUE (NULL, 'Алиса', 'Мэдведь'); 
+INSERT INTO orders VALUE (NULL, 'Ваня', 'Паравозик'); 
+INSERT INTO orders VALUE (NULL, 'Николай', 'Набор Лего'); 
 
-INSERT INTO users (name, password)
-SELECT * FROM (SELECT 'Max', '{SHA}QL0AFWMIX8NRZTKeof9cXsvbvu8=') AS tmp
-WHERE NOT EXISTS (
-    SELECT name FROM users WHERE name = 'Max' AND password= '{SHA}QL0AFWMIX8NRZTKeof9cXsvbvu8='
-) LIMIT 1;
+CREATE TABLE IF NOT EXISTS users (user varchar(191) not null, passwd char(191), primary key (user));
+INSERT INTO users VALUE ('admin', '{SHA}W6ph5Mm5Pz8GgiULbPgzG37mj9g=');
 
-INSERT INTO users (name, password)
-SELECT * FROM (SELECT 'Bob', 'Bob1') AS tmp
-WHERE NOT EXISTS (
-    SELECT name FROM users WHERE name = 'Bob' AND password = 'Bob1'
-) LIMIT 1;
-
-INSERT INTO users (name, password)
-SELECT * FROM (SELECT 'Rover', 'Rover1') AS tmp
-WHERE NOT EXISTS (
-    SELECT name FROM users WHERE name = 'Rover' AND password= 'Rover1'
-) LIMIT 1;
-
-INSERT INTO users (name, password)
-SELECT * FROM (SELECT 'Kate', 'Yandson') AS tmp
-WHERE NOT EXISTS (
-    SELECT name FROM users WHERE name = 'Kate' AND password = 'Yandson'
-) LIMIT 1;
-
-INSERT INTO users (name, password)
-SELECT * FROM (SELECT 'Lilo', 'Black') AS tmp
-WHERE NOT EXISTS (
-    SELECT name FROM users WHERE name = 'Lilo' AND password = 'Black'
-) LIMIT 1;
-
-
-USE appDB;
-CREATE TABLE IF NOT EXISTS books (
-  ID INT(11) NOT NULL AUTO_INCREMENT,
-  title VARCHAR(20) NOT NULL,
-  author VARCHAR(40) NOT NULL,
-  taken tinyint not NULL,
-  PRIMARY KEY (ID)
+CREATE TABLE IF NOT EXISTS uploaded_files (
+    id bigint PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name varchar(64) NOT NULL,
+    type varchar(64) NOT NULL,
+    size int NOT NULL,
+    upload_date datetime NOT NULL
 );
-
-
-INSERT INTO books (title, author,taken)
-SELECT * FROM (SELECT '1984', 'Some Author',0) AS tmp
-WHERE NOT EXISTS (
-    SELECT title FROM books WHERE title = '1984' AND author = 'Some Author'
-) LIMIT 1;
-
-INSERT INTO books (title, author,taken)
-SELECT * FROM (SELECT 'WarandPiece', 'Faty',1) AS tmp
-WHERE NOT EXISTS (
-    SELECT title FROM books WHERE title = 'WarandPiece' AND author = 'Faty'
-) LIMIT 1;
-
-
-USE appDB;
-CREATE TABLE IF NOT EXISTS orders (
-  ID INT(11) NOT NULL AUTO_INCREMENT,
-  book_id INT(20) NOT NULL,
-  date_end TEXT(20) NOT NULL,
-  user_id INT(20) NOT NULL,
-  PRIMARY KEY (ID)
-);
-
-INSERT INTO orders 
-VALUES (1,1,"2022.05.14",1);
